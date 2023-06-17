26d243122b26b4eb67fdef040d8f40d3b40ce153
thesaam <samuel@wedeydo.com>
2023-06-17 04:15:20 +0100

    refractor: delete boilerplate components

---
+++ avatar-forum/src/App.vue
@@ -0,0 +1,124 @@
+<script>
+export default {
+  data: () => ({
+    newCharacter: {
+      name: "",
+      element: []
+    },
+    characterList: [
+      {
+        name: "Aang",
+        element: ["Air", "Water", "Fire", "Earth", "Iron"]
+      },
+      {
+        name: "Zuko",
+        element: ["Water"]
+      },
+      {
+        name: "Toph",
+        element: ["Earth"]
+      },
+      {
+        name: "Katara",
+        element: ["Water"]
+      }
+    ],
+    favoriteList: []
+  }),
+  computed: {
+    benderStatistics() {
+      const elements = ["Air", "Earth", "Fire", "Water", "Iron"]
+      const statistics = {
+        Air: 0,
+        Earth: 0,
+        Water: 0,
+        Fire: 0,
+        Iron: 0
+      }
+
+      this.characterList.forEach(character => {
+        elements.forEach(element => {
+          if (character.element.indexOf(element) > -1) {
+            statistics[element] += 1
+          }
+        })
+      })
+
+      return statistics
+    }
+  },
+  methods: {
+    addNewCharacter() {
+      this.characterList.push(this.newCharacter)
+      this.newCharacter = { name: ""}
+      console.log(this.newCharacter);
+    },
+    addToFav(character) {
+      this.favoriteList.push(character)
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>Statistics</h2>
+  <ul>
+    <li v-for="(stat, type) in benderStatistics" :key="`bender-${stat}-${type}`">
+      {{ type }}: {{ stat }}
+    </li>
+    <!-- {{ benderStatistics }} -->
+  </ul>
+  <h2>Characters</h2>
+  <p v-if="characterList.length === 0">There are no characters</p>
+  <ul v-else-if="characterList.length % 2 === 0">
+    <li v-for="(character,index) in characterList" :key="`even-character-${index}`">
+      <p>{{ character.name }}</p>
+      <button @click="addToFav(character)">⭐Favorite</button>
+    </li>
+  </ul>
+  <p v-else>There are odd characters!</p>
+  <h1>Favorite List</h1>
+  <ul v-if="favoriteList.length > 0">
+    <li v-for="(character,index) in favoriteList" :key="`odd-character-${index}`">{{ character }}</li>
+  </ul>
+  <p v-else>There are no Favorites</p>
+  <h2>New character</h2>
+  <pre>{{ newCharacter }}</pre>
+  <label for="name">Name</label>
+  <input type="text" v-model="newCharacter.name" @keyup.enter="addNewCharacter">
+  <p>
+    <span v-for="(character,index) in characterList" :key="`comma-list-character-${index}`">
+      {{ character.name }}
+      {{ index === characterList.length - 1 ? " " : ", "}}
+    </span>
+  </p>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
