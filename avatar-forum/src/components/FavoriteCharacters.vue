9607969428d5f9c4ef3e297b3a3fc1746d2c5d2d
thesaam <samuel@wedeydo.com>
2023-06-30 19:15:29 +0100

    feature: add favorite cards

---
+++ avatar-forum/src/components/FavoriteCharacters.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data: () => ({
+    favoriteList: [],
+  })
+}
+</script>
+
+<template>
+  <h1>Favorite List</h1>
+  <ul v-if="favoriteList.length > 0">
+    <li v-for="(character,index) in favoriteList" :key="`odd-character-${index}`">{{ character }}</li>
+  </ul>
+  <p v-else>There are no Favorites</p>
+</template>
